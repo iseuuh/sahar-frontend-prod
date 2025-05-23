1efd8fc7b7ce6fba318d32dba4abd866b65f99a7
saharmybae <saharchakroun.rajix@gmail.com>
2025-05-24 00:03:31 +0200

    feat: optimisation pour mobile et connexions lentes

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function ServiceCard({ icon, title, description }) {
   return (
-    <div className="bg-gold text-noir py-3 px-8 rounded-full shadow-lg hover:bg-rose transition">
-      <div className="text-4xl mb-4">{icon}</div>
-      <h3 className="text-xl font-bold mb-2">{title}</h3>
-      <p className="text-center text-base">{description}</p>
+    <div className="bg-noir border border-gold rounded-xl p-6 text-center w-64 hover:shadow-lg transition duration-300">
+      <div className="text-4xl text-gold mb-4">{icon}</div>
+      <h3 className="text-xl font-bold text-gold mb-2">{title}</h3>
+      <p className="text-rose">{description}</p>
     </div>
   );
 }
