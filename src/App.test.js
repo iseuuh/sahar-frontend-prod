c81dcb5fb09eac5f1dceb594c36ac7f0309bef6a
iseuuh <iseuuh@gmail.com>
2025-06-03 23:24:48 +0200

    Nettoyage : suppression des fichiers CRA résiduels après migration Next.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from './pages/Home';
-
-test('Vérifie la présence du titre Sahâr Nail Care', () => {
-  render(<Home />);
-  const titre = screen.getByText(/sahâr nail care/i);
-  expect(titre).toBeInTheDocument();
-});
